7da9d309a011e6482abdc7a3defcf6c37841e182
Marcin Kamiński <kontakt@mkaminski.net>
2021-08-06 07:47:29 +0200

    spring init

---
+++ src/main/java/net/mkaminski/autoManagerBackend/AutoManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package net.mkaminski.autoManagerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoManagerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/mkaminski/autoManagerBackend/AutoManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mkaminski.autoManagerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
