4c53fcec010229cdfe0912610c61f6eb47d5f4c6
Marcin Kamiński <kontakt@mkaminski.net>
2021-08-23 15:51:20 +0200

    add TODO's

--- src/main/java/net/mkaminski/autoManagerBackend/AutoManagerBackendApplication.java
+++ src/main/java/net/mkaminski/autoManagerBackend/AutoManagerBackendApplication.java
@@ -8,8 +8,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 public class AutoManagerBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AutoManagerBackendApplication.class, args);
-	}
+    /*
+     TODO add readme.md
+     TODO add index page
+     TODO add tests
+     TODO change database to postgres
+     TODO  connect postgres on heroku
+     TODO collect stats to the one endpoint
+*/
+    public static void main(String[] args) {
+        SpringApplication.run(AutoManagerBackendApplication.class, args);
+    }
 
 }
